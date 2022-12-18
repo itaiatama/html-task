4489ac326df54f566a85398224aeaecb24a8104a
itaiatama <dieblessrng@gmail.com>
2022-12-18 12:15:40 +0300

    Диплом. React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import Header from "./components/header/Header";
+import Main from "./components/content/Main";
+import Footer from "./components/footer/Footer";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { useState } from "react";
 
-// export default App;
+const App = () => {
+  const [search, setSearch] = useState<string>("");
+
+  const URL = "http://www.last.fm/api/auth/?api_key=fc7e1572ebc98d1718f5aba67915fe76";
+
+  // FIXME: Ugly solution to token generation will be fixed later using react. Maybe.
+  if (!window.location.href.includes("token=")) {
+    window.location.replace(URL);
+  }
+
+  return (
+    <div className="App">
+      <Header
+        setSearch={(value: string | undefined) => {
+          if (value) setSearch(value);
+        }}
+      />
+      <Main isSearch={search !== ""} text={search} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
